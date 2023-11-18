92fdff1d5851fd9b6c233d5fee2964f3943eae66
Daniel Jara <daniel11jara@gmail.com>
2023-11-18 15:26:16 -0300

    aula13

---
+++ agenda/src/main/webapp/scripts/validados.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+function validar(){
+	//variavel nome pegando do formulario o campo nome o seu valor
+	let nome = frmContato.nome.value 
+	let fone = frmContato.fone.value
+	
+	//Passo 3 do diagrama
+	//verificando se os campos estao vazios
+	if(nome === ""){
+		alert('Preencha o campo nome')
+		frmContato.nome.focus() //posicionando o cursor na campo nome
+		return false  //se o usuario nao preencher o formulario nao sera enviado
+	} else if (fone === ""){
+		alert('Preencha o campo Fone')
+		frmContato.nome.focus()
+		return false  
+	} else {
+		document.forms["frmContato"].submit()
+	}
+}
