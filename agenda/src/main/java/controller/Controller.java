2b8732761df8f9ffacec86126eee8797c7b6857b
Daniel Jara <daniel11jara@gmail.com>
2023-11-18 16:06:56 -0300

    aula 14

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -7,14 +7,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import model.DAO;//importando a classe DAO
+import model.JavaBeans;
 
 
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = {"/Controller", "/main", "/insert"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	//criando o objeto DAO
 	DAO dao = new DAO();
+	
+	//criando o objeto no javabeans
+	//esse objeto acessa os metodos publicos dessa classe, os setNome, setFone e setEmail
+	JavaBeans contato = new JavaBeans();
        
     
     public Controller() {
@@ -32,6 +37,10 @@ public class Controller extends HttpServlet {
 		
 		if (action.equals("/main")) {
 			contatos(request, response);
+		}else if (action.equals("/insert")){//se o conteudo da variavel action for insert, vai redirecionar ao metodo responsavel por encaminhar a camda Model
+			novoContato(request, response);
+		}else {
+			response.sendRedirect("index.html");
 		}
 	}
 	
@@ -40,5 +49,16 @@ public class Controller extends HttpServlet {
 		//redirecionando ao documento agenda.jsp
 		response.sendRedirect("agenda.jsp");
 	}
+	//passo 5
+	//Novo contatos
+		protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			System.out.println(request.getParameter("nome"));
+			System.out.println(request.getParameter("fone"));
+			System.out.println(request.getParameter("email"));
+			
+			contato.setNome(request.getParameter("nome"));
+			contato.setFone(request.getParameter("fone"));
+			contato.setEmail(request.getParameter("email"));
+		}
 
 }
